1b6663af429471fa740b71ada0570a20141a4f39
Astrophysics108 <dsingh@perse.co.uk>
2025-12-02 20:59:53 +0000

    initialise macroquad

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+fn conf() -> Conf {
+    Conf {
+        window_title: "Projectile Motion!".to_owned(),
+        window_width: (WIDTH * 2.0) as i32,
+        window_height: (HEIGHT * 2.0) as i32,
+        high_dpi: true,
+        fullscreen: false,
+        window_resizable: false,
+        icon: None,
+        platform: Default::default(),
+        sample_count: 1,
+    }
+}
+
+#[macroquad::main(conf())]
+async fn main() {
+    loop {
+        clear_background(LIGHTGRAY);
+        next_frame().await;
+    }
 }
