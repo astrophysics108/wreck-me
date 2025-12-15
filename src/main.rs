099fc8e8a9a822d15e09cdf72b2914a5596679d2
Astrophysics108 <dsingh@perse.co.uk>
2025-12-15 15:35:19 +0000

    startup screen has animations, move to tutorial

--- src/main.rs
+++ src/main.rs
@@ -29,12 +29,66 @@ fn draw_bg(bg_path: &Texture2D) {
     );
 }
 
+fn is_hovering(x:f32, y: f32, w: f32, h: f32) -> bool {
+    let (mx, my) = mouse_position();
+    if (x - (w/2.0)) <= mx && (x + (w/2.0)) >= mx && (y - (h/2.0)) <= my && (y + (h/2.0)) >= my {
+        return true 
+   }
+   else {
+    return false
+   }
+}
+
 async fn welcome_screen() {
+    let path1 = r"images\wreck_me_one.png";
+    let bg_texture1 = load_texture(&path1).await.unwrap();
+    let path2 = r"images\wreck_me_two.png";
+    let bg_texture2 = load_texture(&path2).await.unwrap();
+
+    let button_path = r"images\button_full.png";
+    let button_texture = load_texture(&button_path).await.unwrap();
+    let button_path_broken = r"images\button_broken.png";
+    let button_texture_broken = load_texture(&button_path_broken).await.unwrap();
     loop {
         clear_background(LIGHTGRAY);
-        let path = r"images\wreck_me_one.png";
-        let bg_texture = load_texture(&path).await.unwrap();
-        draw_bg(&bg_texture);
+        
+        let t = get_time();
+        let bg_texture = if ((t / 0.8).floor() as i32) % 2 == 0 {
+            &bg_texture1
+        } else {
+            &bg_texture2
+        };
+
+        draw_bg(bg_texture);
+
+        let button = if is_hovering(240.0, 295.0, 150.0, 150.0) {
+            &button_texture_broken
+        }
+        else {
+            &button_texture
+        };
+
+        draw_texture_ex(
+            button, 
+            240.0, 295.0,
+            WHITE,
+            DrawTextureParams {
+                dest_size: Some(vec2(150.0, 150.0)),
+                ..Default::default()
+            },
+        );
+
+        if is_mouse_button_pressed(MouseButton::Left) && is_hovering(240.0, 295.0, 150.0, 150.0) {
+            return
+        }
+
+        next_frame().await;
+    }
+}
+
+async fn tutorial() {
+    loop {
+        clear_background(LIGHTGRAY);
         next_frame().await;
     }
 }
@@ -42,4 +96,5 @@ async fn welcome_screen() {
 #[macroquad::main(conf())]
 async fn main() {
     welcome_screen().await;
+    tutorial().await;
 }
