9819f92b26bba7e52495a8ea4e8f1f1e4c005281
Astrophysics108 <dsingh@perse.co.uk>
2025-12-15 14:51:30 +0000

    quick title screen

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use macroquad::prelude::*;
 
+const WIDTH:f32 = 700.0;
+const HEIGHT:f32 = 500.0;
+
 fn conf() -> Conf {
     Conf {
-        window_title: "Projectile Motion!".to_owned(),
-        window_width: (WIDTH * 2.0) as i32,
-        window_height: (HEIGHT * 2.0) as i32,
+        window_title: "Wreck me.".to_owned(),
+        window_width: WIDTH as i32,
+        window_height: HEIGHT as i32,
         high_dpi: true,
         fullscreen: false,
         window_resizable: false,
@@ -14,10 +17,29 @@ fn conf() -> Conf {
     }
 }
 
-#[macroquad::main(conf())]
-async fn main() {
+fn draw_bg(bg_path: &Texture2D) {
+    draw_texture_ex(
+        &bg_path, 
+        0.0, 0.0,
+        WHITE,
+        DrawTextureParams {
+            dest_size: Some(vec2(WIDTH, HEIGHT)),
+            ..Default::default()
+        },
+    );
+}
+
+async fn welcome_screen() {
     loop {
         clear_background(LIGHTGRAY);
+        let path = r"images\wreck_me_one.png";
+        let bg_texture = load_texture(&path).await.unwrap();
+        draw_bg(&bg_texture);
         next_frame().await;
     }
+}
+
+#[macroquad::main(conf())]
+async fn main() {
+    welcome_screen().await;
 }
