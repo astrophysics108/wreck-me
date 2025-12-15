fc3dc17a1b8f24c6f298fa104bdfbca7c5f6d9ca
Astrophysics108 <dsingh@perse.co.uk>
2025-12-15 17:27:22 +0000

    tutorial slides

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,15 @@ fn conf() -> Conf {
     }
 }
 
-fn draw_bg(bg_path: &Texture2D) {
+fn draw_bg(bg_texture1: &Texture2D, bg_texture2: &Texture2D) {
+    let t = get_time();
+    let bg_texture = if ((t / 0.8).floor() as i32) % 2 == 0 {
+            bg_texture1
+        } else {
+            bg_texture2
+    };
     draw_texture_ex(
-        &bg_path, 
+        bg_texture, 
         0.0, 0.0,
         WHITE,
         DrawTextureParams {
@@ -51,15 +57,7 @@ async fn welcome_screen() {
     let button_texture_broken = load_texture(&button_path_broken).await.unwrap();
     loop {
         clear_background(LIGHTGRAY);
-        
-        let t = get_time();
-        let bg_texture = if ((t / 0.8).floor() as i32) % 2 == 0 {
-            &bg_texture1
-        } else {
-            &bg_texture2
-        };
-
-        draw_bg(bg_texture);
+        draw_bg(&bg_texture1, &bg_texture2);
 
         let button = if is_hovering(240.0, 295.0, 150.0, 150.0) {
             &button_texture_broken
@@ -86,11 +84,76 @@ async fn welcome_screen() {
     }
 }
 
-async fn tutorial() {
+async fn tutorial_loading(){
+    let path1 = r"images\tutorial_1.png";
+    let bg_texture1 = load_texture(&path1).await.unwrap();
+    let path2 = r"images\tutorial_2.png";
+    let bg_texture2 = load_texture(&path2).await.unwrap();
+    let t_start = get_time();
+
     loop {
         clear_background(LIGHTGRAY);
+        let t = get_time();
+        if t - t_start >= 4.0 {
+            return
+        };
+
+        draw_bg(&bg_texture1, &bg_texture2);
         next_frame().await;
     }
+}
+
+async fn text_slides_tutorial() {
+    let bg_paths = vec![
+        vec![r"images\tutorial_slides\1_1.png",
+        r"images\tutorial_slides\1_2.png"],
+
+        vec![r"images\tutorial_slides\2_1.png",
+        r"images\tutorial_slides\2_2.png"]
+    ];
+    let mut bg_textures = vec![];
+    let mut paths_collection_to_push = vec![];
+    for paths_collection in bg_paths.iter() {
+        paths_collection_to_push = vec![];
+        for path in paths_collection.iter() {
+            paths_collection_to_push.push(load_texture(&path).await.unwrap());
+        };
+     bg_textures.push(paths_collection_to_push);
+    };
+
+    for slides in bg_textures.iter() {
+        let bg_texture1 = &slides[0];
+        let bg_texture2 = &slides[1];
+        'inner: loop {
+            if is_mouse_button_pressed(MouseButton::Left) {
+                next_frame().await;
+                break 'inner;
+            };
+            clear_background(LIGHTGRAY);
+            draw_bg(&bg_texture1, &bg_texture2);
+            next_frame().await;
+        };
+    }
+}
+
+async fn pick_projectile() {
+    let path1 = r"images\choose_screen.png";
+    let bg_texture1 = load_texture(&path1).await.unwrap();
+    let path2 = r"images\choose_screen_two.png";
+    let bg_texture2 = load_texture(&path2).await.unwrap();
+
+    loop {
+        clear_background(LIGHTGRAY);
+        draw_bg(&bg_texture1, &bg_texture2);
+
+        next_frame().await;
+     }
+}
+
+async fn tutorial() {
+        //tutorial_loading().await;
+        text_slides_tutorial().await;
+        pick_projectile().await;
 }
 
 #[macroquad::main(conf())]
